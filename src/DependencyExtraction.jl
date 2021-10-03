e1d5ad3ec68bb536b39fb6453df82858795b72e2
Alexandre Bergel <alexandre.bergel@me.com>
2021-10-03 07:07:51 -0300

    Working on a visitor

--- src/DependencyExtraction.jl
+++ src/DependencyExtraction.jl
@@ -1,5 +1,43 @@
 module DependencyExtraction
 
-# Write your package code here.
+# EXPORT
+export DEFunction, getName
+export DEModel, numberOfFunctions, importCode
 
+export runOver
+
+
+mutable struct DEFunction
+    name::String
+    beginLOC::Int
+    endLOC::Int
+    outgoingCalls
+end
+DEFunction() = DEFunction("UNKNOWN", -1, -1, [])
+
+getName(f::DEFunction) = f.name
+
+mutable struct DEModel
+    filename::String
+    functions
+end
+DEModel() = DEModel("", [])
+numberOfFunctions(model::DEModel) = length(model.functions)
+
+function importCode(m::DEModel, code::String)
+    expression = Meta.parse(code)
+    println(expression)
+end
+
+
+function runOver(expr::Expr; onCall=(x)->println(x), onValue=(x)->println(x), onSymbol=(x)->println(x))
+    runOver(expr.head, onCall=onCall, onValue=onValue, onSymbol=onSymbol)
+    for i in expr.args
+        runOver(i)
+    end
+end
+
+function runOver(expr::Symbol; onCall=(x)->println(x), onValue=(x)->println(x), onSymbol=(x)->println(x))
+    onSymbol(expr)
+end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using DependencyExtraction
 using Test
 
-@testset "DependencyExtraction.jl" begin
-    # Write your tests here.
+@testset "Basic model" begin
+    model = DEModel()
+    @test numberOfFunctions(model) == 0
+end
+
+
+@testset "Basic function" begin
+    myFunction = DEFunction()
+    @test getName(myFunction) == "UNKNOWN"
+end
+
+@testset "Running a visitor" begin
+    code = "function f()\n    bar()\n    zork()\nend"
+    runOver(Meta.parse(code))
+end
+
+@testset "Importing a function" begin
+    model = DEModel()
+    code = "function f()
+    bar()
+    zork()
+end"
+    importCode(model, code)
+    @test numberOfFunctions(model) == 1
 end
